f6cfa465920231e892015e1b57715e637d760fa0
Douglas Miner <curr3nas@gmail.com>
2020-03-09 10:32:00 -0500

    made the header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './Components/Header'
 
 function App() {
   return (
+    <>
+    <Header />
     <main className='App'>
-      {/* content goes here */}
+      
     </main>
+    </>
   );
 }
 
